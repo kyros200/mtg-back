287226328917e0232d2f3de00af3da16edbf0f36
rafael najjar <rafaelnajjar@gmail.com>
2022-01-09 16:35:46 -0300

    job is moved to once a day again (forgot lol)

--- cron/index.js
+++ cron/index.js
@@ -21,7 +21,7 @@ const nextRequest = async (has_more, next_page) => {
     }
 }
 
-const getAllCards = cron.schedule('* * * * *', async () => {
+const getAllCards = cron.schedule('1 0 * * *', async () => {
     actualRequest = 0;
     console.log("Job has started...")
     setsInfo = (await axios.get('https://api.scryfall.com/sets')).data.data;
