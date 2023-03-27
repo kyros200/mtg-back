b48e5e5d5768523deaedeef2207697baf4b5d22f
rafael najjar <rafaelnajjar@gmail.com>
2023-03-26 23:11:33 -0300

    changed database to rpi

--- database/KnexConnection.js
+++ database/KnexConnection.js
@@ -3,11 +3,11 @@ const knex = require('knex')
 const K = knex({
     client: 'mysql2',
     connection: {
-        host: "kutnpvrhom7lki7u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        host: "rpi.najjar.dev",
         port: 3306,
-        user: "l1jdfx975047recf",
-        password: "ondxky3png17usmd",
-        database: "tr7y4dwbvoy4hfzs"
+        user: "najjar-mtg",
+        password: "senha200",
+        database: "najjar_mtg"
     }
 })
 
